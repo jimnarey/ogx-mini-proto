cff02655245f796ac457ffb28da6dc51f0bff062
Jim Narey <jimnarey@me.com>
2026-02-02 23:33:06 +0000

    Re-enable downloading of pico-sdk

--- Firmware/RP2040/cmake/get_pico_sdk.cmake
+++ Firmware/RP2040/cmake/get_pico_sdk.cmake
@@ -1,7 +1,8 @@
 function(get_pico_sdk EXTERNAL_DIR VERSION_TAG)
     if(NOT DEFINED ENV{PICO_SDK_PATH})
-        message("PICO_SDK_PATH not set")
-        set(PICO_SDK_PATH ${EXTERNAL_DIR}/pico-sdk PARENT_SCOPE)
+        message("PICO_SDK_PATH not set, using ${EXTERNAL_DIR}/pico-sdk")
+        set(PICO_SDK_PATH ${EXTERNAL_DIR}/pico-sdk)
+        set(PICO_SDK_PATH ${PICO_SDK_PATH} PARENT_SCOPE)
 
         if(NOT EXISTS ${PICO_SDK_PATH})
             message("Cloning pico-sdk to ${PICO_SDK_PATH}")
@@ -9,28 +10,28 @@ function(get_pico_sdk EXTERNAL_DIR VERSION_TAG)
                 COMMAND git clone --recursive https://github.com/raspberrypi/pico-sdk.git
                 WORKING_DIRECTORY ${EXTERNAL_DIR}
             )
-
         endif()
 
+        message("Checking out pico-sdk version ${VERSION_TAG}")
         execute_process(
             COMMAND git fetch --tags
-            WORKING_DIRECTORY ENV{PICO_SDK_PATH}
-            )
+            WORKING_DIRECTORY ${PICO_SDK_PATH}
+        )
     
         execute_process(
             COMMAND git checkout tags/${VERSION_TAG}
-            WORKING_DIRECTORY ENV{PICO_SDK_PATH}
-            )
+            WORKING_DIRECTORY ${PICO_SDK_PATH}
+        )
     
         execute_process(
             COMMAND git submodule update --init --recursive 
-            WORKING_DIRECTORY ENV{PICO_SDK_PATH}
-            )
+            WORKING_DIRECTORY ${PICO_SDK_PATH}
+        )
 
     else()
         message("Using PICO_SDK_PATH from environment: $ENV{PICO_SDK_PATH}")
-        set(PICO_SDK_PATH ENV{PICO_SDK_PATH} PARENT_SCOPE)
-
+        set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
+        set(PICO_SDK_PATH ${PICO_SDK_PATH} PARENT_SCOPE)
     endif()
 
     set(PICOTOOL_FETCH_FROM_GIT_PATH ${EXTERNAL_DIR}/picotool PARENT_SCOPE)
